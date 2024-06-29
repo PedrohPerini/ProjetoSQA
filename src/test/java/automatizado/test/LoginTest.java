58a2c5303a7750e38aa30d4317c755cb26a381df
Gabriel nootbook <gablima004@gmail.com>
2024-06-29 05:48:25 -0700

    localhost/nameTest

--- src/test/java/automatizado/test/LoginTest.java
+++ src/test/java/automatizado/test/LoginTest.java
@@ -7,52 +7,47 @@ import org.junit.Test;
 
 import automatizado.page.LoginPO;
 
-public class LoginTest extends baseTest{
-    
+public class LoginTest extends baseTest {
+
     private static LoginPO loginPage;
 
     @BeforeClass
-    public static void iniciarTeste(){
+    public static void iniciarTeste() {
         loginPage = new LoginPO(driver);
 
     }
 
     @Test
-    public void TC001_001_loginValido(){
-        loginPage.inputEmail.sendKeys("admin@admin.com ");
-        loginPage.inputSenha.sendKeys("admin@123");
-        loginPage.btnEntrar.click();
-    }
-
-    @Test
-    public void CT001_003_loginComEmailNaoEncontrado(){
-        loginPage.inputEmail.sendKeys("admin@admin.com ");
-        loginPage.inputSenha.sendKeys("admin@123");
-        loginPage.btnEntrar.click();
-
-    }
-
-    @Test
-    public void CT001_004_loginComSenhaInvalida(){
+    public void TC001_001_loginComEmailNaoEncontrado() {
         loginPage.inputEmail.sendKeys("teste@teste.com ");
         loginPage.inputSenha.sendKeys("admin@123");
         loginPage.btnEntrar.click();
         String mensagem = loginPage.obterResultado();
         assertEquals(mensagem, "E-mail ou senha inválidos");
-
     }
 
     @Test
-    public void CT001_006_usuarioESenhaNull(){
+    public void TC001_002_loginComSenhaInvalida() {
+        loginPage.inputEmail.sendKeys("teste@teste.com ");
+        loginPage.inputSenha.sendKeys("admin@123");
+        loginPage.btnEntrar.click();
+        String mensagem = loginPage.obterResultado();
+        assertEquals(mensagem, "E-mail ou senha inválidos");
+    }
+
+    @Test
+    public void TC001_003_usuarioESenhaNull() {
         loginPage.inputEmail.sendKeys("");
         loginPage.inputSenha.sendKeys("");
         loginPage.btnEntrar.click();
         String mensagem = loginPage.obterResultado();
         assertEquals(mensagem, "Informe usuário e senha, os campos não podem ser brancos.");
-
     }
 
-
-
- 
+    @Test
+    public void TC001_004_loginValido() {
+        loginPage.inputEmail.sendKeys("admin@admin.com ");
+        loginPage.inputSenha.sendKeys("admin@123");
+        loginPage.btnEntrar.click();
+    }
 }
--- src/test/java/automatizado/test/baseTest.java
+++ src/test/java/automatizado/test/baseTest.java
@@ -7,21 +7,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class baseTest {
 
-        protected static WebDriver driver;
-    private static final String URL_Base = "file:///home/gtadmin/Documentos/Estudos/UNIFAA/sistema/login.html";
-    private static final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver";
+    protected static WebDriver driver;
+    private static final String URL_BASE = "http://127.0.0.1:5500/login.html";
+    private static final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver.exe";
 
     @BeforeClass
-    public static void iniciar(){
+    public static void iniciar() {
         System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get(URL_Base);
-        
+        driver.get(URL_BASE);
+
     }
 
     @AfterClass
-    public static void finalizar (){
+    public static void finalizar() {
         driver.quit();
     }
 
